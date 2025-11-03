a55d74ae5805fa2559c06dc5a5888b5e4b0a9e8f
sinyebin <dpqls0625@naver.com>
2025-11-03 15:25:22 +0900

    멘션 시 채널로 메시지

--- server/services/notify/plugindelivery/mention_deliver.go
+++ server/services/notify/plugindelivery/mention_deliver.go
@@ -19,17 +19,27 @@ func (pd *PluginDelivery) MentionDeliver(mentionedUser *mm_model.User, extract s
 		return "", fmt.Errorf("cannot find user: %w", err)
 	}
 
-	channel, err := pd.getDirectChannel(evt.TeamID, mentionedUser.Id, pd.botID)
-	if err != nil {
-		return "", fmt.Errorf("cannot get direct channel: %w", err)
+	// 보드에 연결된 채널이 있으면 그 채널로 메시지 전송, 없으면 DM으로 전송
+	var channelID string
+	if evt.Board.ChannelID != "" {
+		// 보드와 연결된 채널로 메시지 전송
+		channelID = evt.Board.ChannelID
+	} else {
+		// DM 채널로 메시지 전송 (기존 방식)
+		channel, err := pd.getDirectChannel(evt.TeamID, mentionedUser.Id, pd.botID)
+		if err != nil {
+			return "", fmt.Errorf("cannot get direct channel: %w", err)
+		}
+		channelID = channel.Id
 	}
+
 	link := utils.MakeCardLink(pd.serverRoot, evt.Board.TeamID, evt.Board.ID, evt.Card.ID)
 	boardLink := utils.MakeBoardLink(pd.serverRoot, evt.Board.TeamID, evt.Board.ID)
 
 	post := &mm_model.Post{
 		UserId:    pd.botID,
-		ChannelId: channel.Id,
-		Message:   formatMessage(author.Username, extract, evt.Card.Title, link, evt.BlockChanged, boardLink, evt.Board.Title),
+		ChannelId: channelID,
+		Message:   formatMessage(author.Username, mentionedUser.Username, extract, evt.Card.Title, link, evt.BlockChanged, boardLink, evt.Board.Title),
 	}
 
 	if _, err := pd.api.CreatePost(post); err != nil {
